92cdc23de67927168b3f8f8c24de1f963e11c6e0
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2021-11-11 21:44:00 +0530

    returning parsed grid as list of lists

--- chrome_extension/src/board_parser.js
+++ chrome_extension/src/board_parser.js
@@ -33,21 +33,45 @@ export function getBoard() {
 		}
 	}
 
+	function getValueFromCell(dom) {
+		if (dom.firstChild.childElementCount == 0) {
+			return dom.firstChild.textContent;
+		}
+		else if (dom.firstChild.childElementCount == 9) {
+			return ' ';
+		}
+		else {
+			console.error("ERROR: Something unexpected occurred")
+			return ' ';
+		}
+	}
+
+	function getMatrixFromGrid(grid_dom) {
+		console.assert(grid_dom.childElementCount == 81, "ERROR: Exactly 81 elements to be present");
+		var matrix = [];
+		for (let i = 0; i < 9; i++) {
+			let row = [];
+			for (let j = 0; j < 9; j++) {
+				row.push(getValueFromCell(grid_dom.children[9 * i + j]))
+			}
+			matrix.push(row);
+		}
+		return matrix
+	}
+
 	const grid = findGrid(document.body);
 	if (grid == null) {
 		console.warn("WARNING: Grid not found");
-		return false;
+		return null;
 	}
 	if (grid.childElementCount == 81) {
 		console.info("Found Grid");
 		// save the grid
-		// matrix = get_matrix_from_grid(grid)
-		// return matrix
-
-		return true;
+		matrix = getMatrixFromGrid(grid);
+		return matrix;
 	}
 	else {
 		console.warn("WARNING: 81 element grid not found");
-		return false;
+		return null;
 	}
 }
