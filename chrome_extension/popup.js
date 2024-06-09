12cad7662dbe079c4438f14d4f227fb1cdb8304f
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-06-09 16:45:45 +0530

    chrome ext: refactor: made popup.js modular

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -4,13 +4,90 @@ import { solveSudoku } from './src/experimenter.js';
 const swSpan = document.getElementById('supported_websites_span');
 const output = document.getElementById('output');
 
-document.addEventListener('DOMContentLoaded', () => {
+function updateSupportedWebsitesText() {
 	chrome.storage.sync.get('supported_websites', ({ supported_websites }) => {
 		getCurrentHostname().then((hostname) => {
 			swSpan.innerHTML = `Supports ${supported_websites}
         <br/>Current: ${hostname}<br/>`;
 		});
 	});
+}
+
+function renderBoard(foundGrid, hint) {
+	const boardTable = document.createElement('table');
+	boardTable.id = 'sudoku_board';
+
+	for (let [rowIndex, row] of foundGrid.entries()) {
+		const rowElement = document.createElement('tr');
+		rowElement.classList.add('sudoku_row');
+
+		for (let [colIndex, cell] of row.entries()) {
+			const cellElement = document.createElement('td');
+			cellElement.classList.add('sudoku_cell');
+			cellElement.textContent = cell;
+
+			if (hint && (hint.location[0] - 1 === rowIndex) && (hint.location[1] - 1 === colIndex)) {
+				cellElement.classList.add('hint');
+				cellElement.textContent = hint.value;
+			}
+
+			rowElement.appendChild(cellElement);
+		}
+		boardTable.appendChild(rowElement);
+	}
+
+	return boardTable;
+}
+
+function getHint(solvedBoardsWithSteps) {
+	const steps = (solvedBoardsWithSteps.length > 0) ? solvedBoardsWithSteps[0].steps : [];
+	const nextStep = (steps.length > 0) ? steps[0] : null;
+	const hint = nextStep ? {
+		location: nextStep[0],
+		value: nextStep[1],
+		reason: nextStep[2],
+	} : null;
+	return hint;
+}
+
+function renderSolvedBoards(solvedBoardsWithSteps) {
+	const solvedBoardsContainer = document.createElement('div');
+	solvedBoardsContainer.id = 'solved_boards_container';
+
+	solvedBoardsWithSteps.forEach(({ solvedBoard }, i) => {
+		const solvedBoardItem = document.createElement('div');
+		solvedBoardItem.className = 'solved_board_item';
+
+		const titleElement = document.createElement('h4');
+		titleElement.textContent = `Solution #${i + 1} `;
+
+		const solvedBoardTable = document.createElement('table');
+		solvedBoardTable.id = `solved_board_${i + 1} `;
+		solvedBoardTable.className = 'sudoku_solved_board';
+
+		for (let row of solvedBoard) {
+			const rowElement = document.createElement('tr');
+			rowElement.classList.add('sudoku_row');
+
+			for (let cell of row) {
+				const cellElement = document.createElement('td');
+				cellElement.classList.add('sudoku_cell');
+				cellElement.textContent = cell;
+				rowElement.appendChild(cellElement);
+			}
+			solvedBoardTable.appendChild(rowElement);
+		}
+
+		solvedBoardItem.appendChild(titleElement);
+		solvedBoardItem.appendChild(solvedBoardTable);
+		solvedBoardsContainer.appendChild(solvedBoardItem);
+	});
+
+	return solvedBoardsContainer;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	updateSupportedWebsitesText();
 
 	chrome.tabs.query({ active: true, currentWindow: true },
 		(tabs) => {
@@ -22,82 +99,24 @@ document.addEventListener('DOMContentLoaded', () => {
 					const foundGrid = response[0].result;
 					if (!foundGrid) {
 						output.innerHTML = 'Please navigate to the sudoku puzzle screen';
-					} else {
-						const solvedBoardsWithSteps = solveSudoku(foundGrid);
-
-						const steps = (solvedBoardsWithSteps.length > 0) ? solvedBoardsWithSteps[0].steps : [];
-						const nextStep = (steps.length > 0) ? steps[0] : null;
-						const hint = nextStep ? {
-							location: nextStep[0],
-							value: nextStep[1],
-							reason: nextStep[2],
-						} : null;
-
-						const boardTable = document.createElement('table');
-						boardTable.id = 'sudoku_board';
-
-						for (let [rowIndex, row] of foundGrid.entries()) {
-							const rowElement = document.createElement('tr');
-							rowElement.classList.add('sudoku_row');
-
-							for (let [colIndex, cell] of row.entries()) {
-								const cellElement = document.createElement('td');
-								cellElement.classList.add('sudoku_cell');
-								cellElement.textContent = cell;
-
-								if (hint.location[0] - 1 === rowIndex && hint.location[1] - 1 === colIndex) {
-									cellElement.classList.add('hint');
-									cellElement.textContent = hint.value;
-								}
-
-								rowElement.appendChild(cellElement);
-							}
-
-
-							boardTable.appendChild(rowElement);
-						}
-						output.appendChild(boardTable);
-
-						if (hint) {
-							const hintReason = document.createElement('p');
-							hintReason.classList.add('hint_reason');
-							hintReason.textContent = `Next step: Update cell at (${hint.location[0]},${hint.location[1]}) to "${hint.value}" ${hint.reason.toLowerCase()}`;
-							output.appendChild(hintReason);
-						}
-
-						const solvedBoardsContainer = document.createElement('div');
-						solvedBoardsContainer.id = 'solved_boards_container';
-
-						solvedBoardsWithSteps.forEach(({ solvedBoard }, i) => {
-							const solvedBoardItem = document.createElement('div');
-							solvedBoardItem.className = 'solved_board_item';
-
-							const titleElement = document.createElement('h4');
-							titleElement.textContent = `Solution #${i + 1} `;
-
-							const solvedBoardTable = document.createElement('table');
-							solvedBoardTable.id = `solved_board_${i + 1} `;
-							solvedBoardTable.className = 'sudoku_solved_board';
-
-							for (let row of solvedBoard) {
-								const rowElement = document.createElement('tr');
-								rowElement.classList.add('sudoku_row');
-
-								for (let cell of row) {
-									const cellElement = document.createElement('td');
-									cellElement.classList.add('sudoku_cell');
-									cellElement.textContent = cell;
-									rowElement.appendChild(cellElement);
-								}
-								solvedBoardTable.appendChild(rowElement);
-							}
-
-							solvedBoardItem.appendChild(titleElement);
-							solvedBoardItem.appendChild(solvedBoardTable);
-							solvedBoardsContainer.appendChild(solvedBoardItem);
-						});
-						output.appendChild(solvedBoardsContainer);
+						return;
 					}
+
+					const solvedBoardsWithSteps = solveSudoku(foundGrid);
+					const hint = getHint(solvedBoardsWithSteps);
+
+					const boardTable = renderBoard(foundGrid, hint);
+					output.appendChild(boardTable);
+
+					if (hint) {
+						const hintReason = document.createElement('p');
+						hintReason.classList.add('hint_reason');
+						hintReason.textContent = `Next step: Update cell at (${hint.location[0]},${hint.location[1]}) to "${hint.value}" ${hint.reason.toLowerCase()}`;
+						output.appendChild(hintReason);
+					}
+
+					const solvedBoardsContainer = renderSolvedBoards(solvedBoardsWithSteps);
+					output.appendChild(solvedBoardsContainer);
 				});
 		});
 });
