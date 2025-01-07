4750e3bac45d4afa25392976a2335f498bc23a94
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2025-01-07 18:31:09 +0530

    chrome_ext: removed the tabs permission and aded host_permissions for all urls

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,5 +1,3 @@
-import { getCurrentHostname } from './src/board_parser.js'
-
 let supported_websites = ['www.usdoku.com'];
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -9,16 +7,36 @@ chrome.runtime.onInstalled.addListener(() => {
 	chrome.action.setTitle({ title: 'Sudoku Solver\nList of supported sudoku websites: ' + supported_websites });
 });
 
-chrome.tabs.onActivated.addListener(() => {
-	chrome.storage.sync.get("supported_websites", ({ supported_websites }) => {
-		getCurrentHostname().then((hostname) => {
-			if (supported_websites.indexOf(hostname) != -1) {
-				chrome.action.enable();
-				chrome.action.setTitle({ title: 'Sudoku Solver' });
-			} else {
-				chrome.action.disable();
-				chrome.action.setTitle({ title: 'Sudoku Solver\nList of supported sudoku websites: ' + supported_websites });
-			}
-		});
-	});
+function updateIcon(tab) {
+	if (tab && tab.url) {
+		try {
+			const url = new URL(tab.url);
+			const hostname = url.hostname;
+			chrome.storage.sync.get("supported_websites", ({ supported_websites }) => {
+				if (supported_websites && supported_websites.includes(hostname)) {
+					chrome.action.enable(tab.id);
+					chrome.action.setTitle({ tabId: tab.id, title: 'Sudoku Solver' });
+				} else {
+					chrome.action.setTitle({ tabId: tab.id, title: 'Sudoku Solver\nList of supported sudoku websites: ' + supported_websites });
+				}
+			});
+		} catch (error) {
+			// Handle invalid URLs
+			chrome.action.disable();
+			chrome.action.setTitle({ tabId: tab.id, title: '[Sudoku Solver] Invalid URL' });
+		}
+	} else {
+		chrome.action.disable();
+		chrome.action.setTitle({ tabId: tab.id, title: 'Sudoku Solver could not read tab\'s URL' });
+	}
+}
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	chrome.tabs.get(activeInfo.tabId, updateIcon);
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo.status === 'complete') { // Only check when the page has finished loading
+		updateIcon(tab);
+	}
 })
